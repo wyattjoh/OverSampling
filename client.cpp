626e0314d7bd9347c2c538b5bbd14a01d0f48353
Wyatt Johnson <wyatt@wyattjohnson.ca>
2013-11-28 18:23:08 -0700

    Cleaned up

--- client.cpp
+++ client.cpp
@@ -17,13 +17,7 @@ void setup()
 // the loop routine runs over and over again forever:
 void loop()
 {
-	// // read the input on analog pin 0:
-	// int sensorValue = analogRead(A0);
-	// // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-	// float voltage = sensorValue * (5.0 / 1023.0);
-	// // print out the value you read:
-	// Serial.println(voltage);
-
+	// Static... dont worry about it
 	static int i;
 
 	static int sensorValue;
@@ -34,14 +28,13 @@ void loop()
 	tallies = 0;
 	mill = millis();
 
-
 	if (mill - last_time >= 1000) {
 		last_time = mill;
 		for (i = 0; i < measurement_samples; i++) {
+			// read the input on analog pin 0:
 			tallies += analogRead(A0);
 		}
 
 		Serial.println(tallies << 2, BIN);
 	}
-
 }
